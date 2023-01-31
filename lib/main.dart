61337d1d1cea8df1bb19a580a3934d29a135ee4d
bhav-khurana <bhav.khurana.cse21@itbhu.ac.in>
2023-01-31 16:47:28 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:exif/exif.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_phoenix/flutter_phoenix.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:image/image.dart' as im;
 import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:image_picker/image_picker.dart';
@@ -13,23 +14,81 @@ import 'package:flutter_background_service/flutter_background_service.dart';
 import 'package:photo_gallery/photo_gallery.dart';
 import 'dart:async';
 import 'package:shared_preferences/shared_preferences.dart';
-
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:photo_resizer/notification service.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
 void main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
+  MobileAds.instance.updateRequestConfiguration(
+    RequestConfiguration(
+      tagForChildDirectedTreatment:
+      TagForChildDirectedTreatment.unspecified,
+      testDeviceIds: <String>[
+        "EA645858860344369B519254C5014A06",
+      ],
+    ),
+  );
+  NotificationService().initNotification();
   runApp(Phoenix(child: const MyApp()));
 
 }
 
 void requestPermission() async {
-  var status = await Permission.storage.status;
-  if (!status.isGranted) {
-    await Permission.storage.request();
+  if (Platform.isAndroid){
+    var status = await Permission.storage.status;
+    if (!status.isGranted) {
+      await Permission.storage.request();
+    }
+    var status1 = await Permission.manageExternalStorage.status;
+    if (!status1.isGranted) {
+      await Permission.manageExternalStorage.request();
+    }
+  } else {
+    var status = await Permission.photos.status;
+    if (!status.isGranted) {
+      await Permission.photos.request();
+    }
+    var status1 = await Permission.manageExternalStorage.status;
+    if (!status1.isGranted) {
+      await Permission.manageExternalStorage.request();
+    }
   }
-  var status1 = await Permission.manageExternalStorage.status;
-  if (!status1.isGranted) {
-    await Permission.manageExternalStorage.request();
-  }
+}
+
+Future<void> showPopUp(BuildContext context, BannerAd myBanner) async {
+  print('start');
+  final prefs = await SharedPreferences.getInstance();
+  await Future.delayed(const Duration(seconds: 1)).then((value) {
+    print('end');
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return SimpleDialog(
+          // title: Text('You have resized ${prefs.getInt('photosResized') ?? 0} photo(s) in the last 14 days'),
+          children: [
+            Container(
+              height: 50,
+              width: 200,
+              child: AdWidget(ad: myBanner,),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('You have resized ${prefs.getDouble('photosResized') ?? 0} photo(s) in the last 14 days'),
+            ),
+            const SizedBox(height: 20,),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('You saved ${(prefs.getDouble('sizeSaved') ?? 0).toStringAsFixed(2)} MB of space '),
+            ),
+
+          ],
+        );
+      }
+    );
+  });
 }
 
 Future<bool> initAsync() async {
@@ -47,29 +106,23 @@ Future<bool> initAsync() async {
     return true;
   }
   else {
-    bool p1 = await Permission.storage.request().isGranted;
-    if (p1) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-}
+    PermissionStatus permissionStatus; // note do not use PermissionStatus? permissionStatus;
 
-Future<bool> _promptPermissionSetting() async {
-  if (Platform.isIOS && await Permission.storage.request().isGranted &&
-      await Permission.photos.request().isGranted ||
-      Platform.isAndroid && await Permission.storage.request().isGranted && await Permission.notification.request().isGranted) {
+    while (true) {
+      try {
+        permissionStatus = await Permission.photos.request();
+        break;
+      } catch (e) {
+        await Future.delayed(const Duration(milliseconds: 500), () {});
+      }
+    }
     return true;
   }
-  return false;
 }
-
 
 void onStart() async {
 
   int prevLen = 12893;
-
   WidgetsFlutterBinding.ensureInitialized();
   final service = FlutterBackgroundService();
 
@@ -77,6 +130,7 @@ void onStart() async {
     print(image.path);
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     await prefs.reload();
+    bool toDelete = prefs.getBool('deleteOriginal') ?? false;
     print('done');
     print(prefs.getInt('bHeight'));
     int? bh = prefs.getInt('bHeight')!;
@@ -86,9 +140,19 @@ void onStart() async {
     im.Image? img = im.decodeJpg(File(image.path).readAsBytesSync());
     im.Image thumbnail = im.copyResize(img!, width: ((bh/100)*img.width).round(), height: ((bh/100)*img.height).round());
     final f = await File('/data/user/0/com.example.photo_resizer/cache/y${DateTime.now()}.jpg').writeAsBytes(im.encodeJpg(thumbnail));
-    ImageGallerySaver.saveFile(f.path).then((value) => print('Saved ✅'));
-    File(image.path).deleteSync();
-
+    await ImageGallerySaver.saveFile(f.path).then((value) => print('Saved ✅'));
+    double photosResized = prefs.getDouble('photosResized') ?? 0;
+    if (toDelete) {
+      await prefs.setDouble('photosResized', photosResized+1);
+      double sz = File(image.path).lengthSync().toDouble();
+      double saved = ((100-bh)/100)*sz;
+      double sizeSaved = prefs.getDouble('sizeSaved') ?? 0;
+      await prefs.setDouble('sizeSaved', sizeSaved+saved/(1024*1024));
+      File(image.path).deleteSync();
+    }
+    else {
+      await prefs.setDouble('photosResized', photosResized+1);
+    }
   }
 
   Future<MediaPage> getImagesFromGallery() async {
@@ -106,19 +170,28 @@ void onStart() async {
     print(event!);
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     await prefs.reload();
+    tz.initializeTimeZones();
     if (event.containsKey('action')) {
+      int val = 0;
       while (true) {
         getImagesFromGallery().then((value) async {
           if (value.items.length > prevLen) {
             await resizeImage((await value.items.last.getFile()));
-            service.setNotificationInfo(
-              title: "Running",
-              content: "Updated at ${DateTime.now()}",
-            );
+            // service.setNotificationInfo(
+            //   title: "Running",
+            //   content: "Updated at ${DateTime.now()}",
+            // );
           }
-          prevLen = value.items.length ;
+          prevLen = value.items.length;
         });
-        await Future.delayed(const Duration(seconds: 10));
+        await Future.delayed(const Duration(seconds: 4));
+        await prefs.reload();
+        val += 4;
+        if(val % 60 == 0) {
+            NotificationService().showNotification(
+              1, 'Number of Photos Resized', ((prefs.getDouble('photosResized') ?? 0.0).round()).toString()
+          );
+        }
       }
     }
   });
@@ -139,6 +212,7 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
+    tz.initializeTimeZones();
     final prefs = SharedPreferences.getInstance();
     opened = prefs.then((SharedPreferences prefs) {
       return prefs.getBool('opened') ?? false;
@@ -241,23 +315,45 @@ class MyHome extends StatefulWidget {
 
 class _MyHomeState extends State<MyHome> {
   final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
-
+  final BannerAd myBanner = BannerAd(
+    adUnitId: Platform.isAndroid ? '	ca-app-pub-3940256099942544/6300978111' : 'ca-app-pub-3940256099942544/2934735716',
+    size: AdSize.banner,
+    request: const AdRequest(),
+    listener: const BannerAdListener(),
+  );
   bool _loading = false;
   List _albums = [];
   late Future<bool> isOn;
+  late Future<bool> deleteOriginal;
 
   @override
   void initState() {
     super.initState();
+    WidgetsFlutterBinding.ensureInitialized();
+    MobileAds.instance.initialize();
+    MobileAds.instance.updateRequestConfiguration(
+      RequestConfiguration(
+        tagForChildDirectedTreatment:
+        TagForChildDirectedTreatment.unspecified,
+        testDeviceIds: <String>[
+          "EA645858860344369B519254C5014A06",
+        ],
+      ),
+    );
+    myBanner.load();
     _loading = true;
     isOn = _prefs.then((SharedPreferences prefs) {
       return prefs.getBool('isOn') ?? false;
+    });
+    deleteOriginal = _prefs.then((SharedPreferences prefs) {
+      return prefs.getBool('deleteOriginal') ?? false;
     });
     requestPermission();
     FlutterBackgroundService.initialize(onStart);
     FlutterBackgroundService().sendData({
       'action': 'action'
     });
+    showPopUp(context, myBanner);
     // FlutterBackgroundService.initialize(onStart);
   }
 
@@ -276,7 +372,15 @@ class _MyHomeState extends State<MyHome> {
 
       });
     });
+  }
 
+  Future<void> swapDeleteOriginal(bool value) async {
+    final SharedPreferences prefs = await _prefs;
+    setState(() {
+      deleteOriginal = prefs.setBool('deleteOriginal', value).then((val) async {
+        return value;
+      });
+    });
   }
 
   SingleValueDropDownController bHeightController = SingleValueDropDownController();
@@ -337,7 +441,7 @@ class _MyHomeState extends State<MyHome> {
                   enableSearch: true,
                   dropDownItemCount: 100,
                   dropDownList: [
-                    for (int i = 10; i <= 50; i+=10)
+                    for (int i = 5; i <= 50; i+=5)
                       DropDownValueModel(name: '${2*i}%', value: i.toString())
                   ],
                 ),
@@ -353,7 +457,31 @@ class _MyHomeState extends State<MyHome> {
                   child: const Text('Set'),
                 ),
               ),
-              const SizedBox(height: 40,),
+              Center(
+                child: FutureBuilder(
+                  future: deleteOriginal,
+                  builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+                    switch (snapshot.connectionState) {
+                      case ConnectionState.waiting:
+                        return const CircularProgressIndicator();
+                      default:
+                        if (snapshot.hasError) {
+                          return Text('Error: ${snapshot.error}');
+                        } else {
+                          return Switch(
+                            value: snapshot.data!,
+                            onChanged: (value) async {
+                              print(value);
+                              await swapDeleteOriginal(value);
+                            },
+                          );
+                        }
+                    }
+                  },
+                ),
+              ),
+              const Center(child: Text('Delete Original Image from Gallery')),
+              const SizedBox(height: 50,),
               const Text('Crop an Image from Gallery', style: TextStyle(fontSize: 16),),
               Padding(
                 padding: const EdgeInsets.symmetric(vertical: 16.0),
---
+++ lib/notification service.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:timezone/timezone.dart' as tz;
+
+class NotificationService {
+  static final NotificationService _notificationService =
+  NotificationService._internal();
+
+  factory NotificationService() {
+    return _notificationService;
+  }
+
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+  FlutterLocalNotificationsPlugin();
+
+  NotificationService._internal();
+
+  Future<void> initNotification() async {
+
+    // Android initialization
+    final AndroidInitializationSettings initializationSettingsAndroid =
+    AndroidInitializationSettings('@mipmap/ic_launcher');
+
+    // ios initialization
+    const DarwinInitializationSettings initializationSettingsIOS =
+    DarwinInitializationSettings(
+      requestAlertPermission: false,
+      requestBadgePermission: false,
+      requestSoundPermission: false,
+    );
+
+    final InitializationSettings initializationSettings =
+    InitializationSettings(
+        android: initializationSettingsAndroid,
+        iOS: initializationSettingsIOS);
+    // the initialization settings are initialized after they are set
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  }
+
+  Future<void> showNotification(int id, String title, String body) async {
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+      id,
+      title,
+      body,
+      tz.TZDateTime.now(tz.local).add(const Duration(
+          seconds: 1)),
+      const NotificationDetails(
+        // Android details
+        android: AndroidNotificationDetails('main_channel', 'Main Channel',
+            ongoing: true,
+            channelDescription: "ashwin",
+            importance: Importance.max,
+            priority: Priority.max),
+        // iOS details
+        iOS: DarwinNotificationDetails(
+          sound: 'default.wav',
+          presentAlert: true,
+          presentBadge: true,
+          presentSound: true,
+        ),
+      ),
+
+      // Type of time interpretation
+      uiLocalNotificationDateInterpretation:
+      UILocalNotificationDateInterpretation.absoluteTime,
+      androidAllowWhileIdle:
+      true, // To show notification even when the app is closed
+    );
+    // const AndroidNotificationDetails androidNotificationDetails =
+    // AndroidNotificationDetails(
+    //     'repeating channel id', 'repeating channel name',
+    //     ongoing: true,
+    //     channelDescription: 'repeating description');
+    // const NotificationDetails notificationDetails =
+    // NotificationDetails(android: androidNotificationDetails);
+    // await flutterLocalNotificationsPlugin.periodicallyShow(id, title,
+    //     body, RepeatInterval.everyMinute, notificationDetails,
+    //     androidAllowWhileIdle: true).then((e) async {
+    //       final prefs = await SharedPreferences.getInstance();
+    //       await prefs.reload();
+    // });
+  }
+}
